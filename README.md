267a6c6843a8c856fca251f92e498a69610ef058
emerick42 <emerick42@gmail.com>
2013-06-19 16:23:08 +0200

    Remove composer.phar, add Capifony deployment file, update the README

--- README.md
+++ README.md
@@ -2,24 +2,55 @@
 
 A Symfony2 distrib improved with FOSUserBundle, DoctrineFixtures, DoctrineExtensions and DoctrineMigrations. It also includes Yui-compressor. Everything is preconfigured as possible.
 
-You just have to follow these short steps to start your project:
- * copy the app/config/parameter.yml.dist and fill with your database information
- * install the vendor with composer
- * reconfigure the secured area (default is /admin) and modify the prefix of the FOSUserBundle routes in app/config/routing.yml 
- * create your own bundle
- * create the User entity in your bundle : [Read the FOSUserBundle Documentation](https://github.com/FriendsOfSymfony/FOSUserBundle/blob/master/Resources/doc/index.md)
- * change default User class entry in app/config/config.yml under fos_user 
- * if you use Assetic add your bundle in its configuration in app/config/config.yml
+##Features
 
+This distribution allows you to start your Symfony2 projects really fast. You can just fork this repository and reconfigure it as you want.
+Here is a list of the main features:
+* uses one of the last stable version of Symfony2
+* includes useful bundles like FOSUserBundle, DoctrineFixtures, DoctrineExtensions and DoctrineMigrations
+* includes Assetic to manage your resources
+* includes the java binary for Yui-compressor to compress your JS and CSS files with Assetic
+* reconfigures user's sessions to keep them after cache:clear
+* includes a default Capifony deployment file to handle Assetic, migrations and user's session
+* automatically creates a parameter.yml that you have to fill after a Capifony deploy:setup
+
+##Installation and configuration
+
+###First things you have to do
+
+You just have to follow these short steps to start your project:
+* copy the app/config/parameter.yml.dist and fill with your database information
+* install the vendor with composer
+* reconfigure the secured area (default is /admin) and modify the prefix of the FOSUserBundle routes in app/config/routing.yml 
+* create your own bundle
+* create the User entity in your bundle : [Read the FOSUserBundle Documentation](https://github.com/FriendsOfSymfony/FOSUserBundle/blob/master/Resources/doc/index.md)
+* change default User class entry in app/config/config.yml under fos_user 
+* add your bundle in Assetic's configuration in app/config/config.yml
+
+You can also see the optional configurations below to take the best of this distribution.
 I hope this will help kickstart your projects.
+
+###Optional configurations
+
+####Use Capifony
+
+If you want to use Capifony, this distribution includes a default configuration file.
+There are some mandatory information to provide before using Capifony with this file:
+* change your _application name_, _domain name_, _deploy path_, _username_ and _repository address_
+
+You can also change every Capifony settings you want to meet your needs.
+It's basically just an usual Capifony deployment file but configured to handle Assetic, migrations and user's sessions.
 
 ##Changelog
 
+_**develop**_
+* Add a default Capifony configuration file
+
 _2013/04/05: **v1.1.2**_
- * Add Doctrine migrations to Composer dependencies
+* Add Doctrine migrations to Composer dependencies
 
 _2013/03/20: **v1.1.1**_
- * Update config.yml to use default locale from parameters.yml
+* Update config.yml to use default locale from parameters.yml
 
 _2013/03/11: **v1.1.0**_
- * Now using Symfony 2.2
+* Now using Symfony 2.2
---
+++ app/config/deploy.rb
@@ -0,0 +1,128 @@
+default_run_options[:pty] = true
+
+set :application, "sid"
+set :domain,      "your-domain.com"
+set :deploy_to,   "/path/to/deploy"
+set :app_path,    "app"
+
+set :user,        "username"
+ssh_options[:forward_agent] = true
+set :use_sudo, false
+
+set :repository,  "git@your-domain.com:sid.git"
+set :scm,         :git
+set :branch,      "master"
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `subversion`, `mercurial`, `perforce`, or `none`
+
+set :model_manager, "doctrine"
+# Or: `propel`
+
+role :web,        domain                         # Your HTTP server, Apache/etc
+role :app,        domain, :primary => true       # This may be the same as your `Web` server
+role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
+
+set  :keep_releases,  1
+
+# Be more verbose by uncommenting the following line
+# logger.level = Logger::MAX_LEVEL
+
+logger.level = Logger::MAX_LEVEL
+
+set :writable_dirs, [app_path + "/cache", app_path + "/logs"]
+set :webserver_user, "www-data"
+set :permission_method, :acl
+before "deploy:restart", "deploy:set_permissions"
+
+set :use_composer, true
+set :update_vendors, true
+
+set :shared_files, ["app/config/parameters.yml"]
+set :shared_children, [web_path + "/uploads", app_path + "/var"]
+set :writable_shared_dirs, [web_path + "/uploads", app_path + "/var"]
+
+# Add parameters.yml
+task :upload_parameters do
+    origin_file = "app/config/parameters.yml"
+    destination_file = shared_path + "/app/config/parameters.yml"
+    try_sudo "mkdir -p #{File.dirname(destination_file)}"
+    top.upload(origin_file, destination_file)
+end
+after "deploy:setup", "upload_parameters"
+
+before "symfony:cache:warmup", "symfony:doctrine:migrations:migrate"
+
+before "symfony:cache:warmup", "deploy:assetic_dump"
+
+namespace :deploy do
+    task :assetic_dump do
+        run "php #{latest_release}/app/console assetic:dump --env=prod"
+    end
+end
+
+set :dev_only_files,    [web_path + "/app_dev.php", web_path + "/check.php", web_path + "/config.php"]
+set :production, true
+ 
+after 'symfony:cache:warmup', 'pff:productify'
+ 
+namespace :pff do
+    desc "Remove app_dev.php, check.php and config.php from production deployment"
+    task :productify, :except => { :production => false } do
+        if dev_only_files
+            dev_only_files.map do |dev_file|
+                run "rm -f #{latest_release}/#{dev_file}"
+            end
+        end
+    end
+end
+
+after 'deploy:setup', 'sid:set_shared_folders_permissions'
+# This function is a copy of the set_permissions function to handle shared children's writable permissions
+namespace :sid do
+    task :set_shared_folders_permissions, :roles => :app, :except => { :no_release => true } do
+        if writable_shared_dirs && permission_method
+            dirs = []
+        writable_shared_dirs.each do |link|
+            if shared_children && shared_children.include?(link)
+                absolute_link = shared_path + "/" + link
+            else
+                absolute_link = latest_release + "/" + link
+            end
+            dirs << absolute_link
+        end
+        methods = {
+            :chmod => [
+                "chmod +a \"#{user} allow delete,write,append,file_inherit,directory_inherit\" %s",
+                "chmod +a \"#{webserver_user} allow delete,write,append,file_inherit,directory_inherit\" %s"
+            ],
+            :acl   => [
+                "setfacl -R -m u:#{user}:rwX -m u:#{webserver_user}:rwX %s",
+                "setfacl -dR -m u:#{user}:rwx -m u:#{webserver_user}:rwx %s"
+            ],
+            :chown => ["chown #{webserver_user} %s"]
+        }
+        if methods[permission_method]
+            capifony_pretty_print "--> Setting permissions"
+            if fetch(:use_sudo, false)
+                methods[permission_method].each do |cmd|
+                    sudo sprintf(cmd, dirs.join(' '))
+                end
+            elsif permission_method == :chown
+                puts "    You can't use chown method without sudoing"
+            else
+                dirs.each do |dir|
+                    is_owner = (capture "`echo stat #{dir} -c %U`").chomp == user
+                    if is_owner && permission_method != :chown
+                        methods[permission_method].each do |cmd|
+                            try_sudo sprintf(cmd, dir)
+                        end
+                    else
+                        puts "    #{dir} is not owned by #{user} or you are using 'chown' method without ':use_sudo'"
+                    end
+                end
+            end
+            capifony_puts_ok
+        else
+            puts "    Permission method '#{permission_method}' does not exist.".yellow
+        end
+    end
+end
